f567473be4d1beaac8e0a3db9f159d9b0cd220bb
Kevin Lin <kevinptt0323@gmail.com>
2018-11-25 13:56:47 +0800

    remove comments

--- np_single_proc/main.cpp
+++ np_single_proc/main.cpp
@@ -70,51 +70,6 @@ char* input_command(int fd, char* buf, int bufsize) {
 	return buf;
 }
 
-/*
-void remote_shell(int client_sockfd) {
-
-	job("setenv PATH bin:.").exec();
-
-	extern bool exit_flag;
-	exit_flag = false;
-
-	while (!exit_flag) {
-		print_prompt(client_sockfd, prompt);
-		if (input_command(client_sockfd, buf, BUF_SIZE)) {
-			if (strlen(buf) > 0) {
-				number_pipe_manager.reduce_count();
-				job curr_job(buf);
-				curr_job.set_stdin_fileno(client_sockfd);
-				curr_job.set_stdout_fileno(client_sockfd);
-				curr_job.set_stderr_fileno(client_sockfd);
-				const Fd2* pipe_in = number_pipe_manager.get_fd2(0);
-				if (pipe_in) {
-					curr_job.pipe_in = move(*pipe_in);
-					number_pipe_manager.pop_front();
-				}
-				bool same_pipe_next_n = false;
-				if (curr_job.pipe_next_n > 0) {
-					const Fd2* pipe_out = number_pipe_manager.get_fd2(curr_job.pipe_next_n);
-					if (pipe_out) {
-						same_pipe_next_n = true;
-						curr_job.pipe_out = move(*pipe_out);
-					}
-				}
-				curr_job.exec();
-				if (curr_job.pipe_next_n > 0 && !same_pipe_next_n) {
-					number_pipe_manager.set_fd2(curr_job.pipe_next_n, curr_job.pipe_out);
-				}
-			}
-		} else {
-			break;
-		}
-	}
-
-	number_pipe_manager.close();
-	close(client_sockfd);
-}
-*/
-
 UserManager user_manager;
 
 int bin_who(
@@ -229,6 +184,7 @@ int main(int argc, char* argv[]) {
 					stringstream ss;
 					ss << "*** User '" << user.nickname << "' entered from " << user.addr << ". ***" << endl;
 					user_manager.broadcast(ss.str());
+					// job("setenv PATH bin:.").exec();
 					print_prompt(client_sockfd, prompt);
 				} else {
 					int client_sockfd = i;
